77104cfab063ef1254c8a4f2f13b6e7d4f6cf900
Facundo <fcreus@gmail.com>
2021-12-26 12:58:58 -0300

    Feat Info Endpoint + Swagger Documentation.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import authRouter from './routerJwtAuth';
 import productsRouter from './routerProducts';
 import imagesRouter from './routerProductImages';
 import cartsRouter from './routerCarts';
+import infoRouter from './routerInfo';
 const router = Router();
 
 router.use('/user', authRouter);
 router.use('/products', productsRouter);
 router.use('/images', imagesRouter);
 router.use('/carts', cartsRouter);
+router.use('/info', infoRouter);
 
 export default router;
